cefd4db463dbe5db1c870e251f9aabd1edf84e04
FredyVides <fredyvides@gmail.com>
2021-09-01 22:32:08 +0000

    Updated Python folder

--- Python/SpAutoencoder.py
+++ Python/SpAutoencoder.py
@@ -15,13 +15,20 @@ from matplotlib.pyplot import plot, stem, show
 from numpy import reshape
 data = pandas.read_csv('../DataSets/signal_with_anomaly.csv', usecols=[1], 
                        engine='python')
+data = data.values[:,0]
 from SpAutoencoder import SpAutoencoder
 W = SpAutoencoder(data,300,600,1e2)
 h = reshape(data[:300],(300,1))
+t = reshape(data[2900:(2900+300)],(300,1))
 hw = W.T@h
+tw  = W.T@t
 plot(W@hw),plot(h)
 show()
-stem(abs(W.T@h))
+plot(W@hw),plot(h)
+show()
+stem(abs(hw))
+show()
+stem(abs(tw))
 """
 def SpAutoencoder(data,L,N,tol=1e-2):
     from numpy.linalg import svd,lstsq,norm
