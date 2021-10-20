8b8edb2aa81e26e86a87747b4b46afe038beabb5
FredyVides <fredyvides@gmail.com>
2021-10-20 18:17:01 +0000

    Updated SPAAR repository

--- Python/SpGRUModelTorch.py
+++ Python/SpGRUModelTorch.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-=======
-# -*- coding: utf-8 -*-
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
 """
 Created on Wed Mar 31 02:57:52 2021
 SPGRUMODEL GRU based sparse signal model
    Code by Fredy Vides
-<<<<<<< HEAD
    For Paper, "Computing Sparse Semilinear Models for Approximately Eventually Periodic Signals"
-=======
-   For Paper, "Computing Sparse Autoencoders and Autoregressors for Signal Identification"
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
    by F. Vides
 @author: Fredy Vides
 """
@@ -40,7 +32,6 @@ def SpGRUModelTorch(data,Lag,sp,N,ep,spp = 0.1):
     Ytt = []
     for k in range(Xt.shape[0]-1):
         Xtt.append(torch.FloatTensor(reshape(Xt[k],(1,1,Lag))))
-<<<<<<< HEAD
         Ytt.append(torch.FloatTensor(reshape(Yt[k],(1,1,1))))
     
     class GRU_Adapter(nn.Module):
@@ -56,30 +47,6 @@ def SpGRUModelTorch(data,Lag,sp,N,ep,spp = 0.1):
         GRU_Adapter(1),
         torch.nn.Linear(N,1)
         )
-=======
-        Ytt.append(torch.FloatTensor(reshape(Yt[k],(1,1))))
-     
-    class GRU(nn.Module):
-        def __init__(self, num_classes, input_size, hidden_size, num_layers):
-            super(GRU, self).__init__()
-            self.num_classes = num_classes
-            self.num_layers = num_layers
-            self.input_size = input_size
-            self.hidden_size = hidden_size
-           
-            self.gru = nn.GRU(input_size=input_size, hidden_size=hidden_size,
-                            num_layers=num_layers, batch_first=True)
-            self.linear = nn.Linear(hidden_size, num_classes)
-        
-        def forward(self, x):
-            h_0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)  
-            _, h_out = self.gru(x, h_0)
-            h_out = h_out.view(-1, self.hidden_size)
-            out = self.linear(h_out)   
-            return out
-    
-    model = GRU(1,Lag,N,1)
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
     loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=spp)
     
@@ -88,11 +55,6 @@ def SpGRUModelTorch(data,Lag,sp,N,ep,spp = 0.1):
     while i <= ep and error>10*spp:
         for k in range(len(Ytt)):
             optimizer.zero_grad()
-<<<<<<< HEAD
-=======
-            model.hidden_cell = (torch.zeros(1, 1, model.hidden_size),
-                        torch.zeros(1, 1, model.hidden_size))
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
             y_pred = model(Xtt[k])
             single_loss = loss_function(y_pred, Ytt[k])
             single_loss.backward()
--- Python/SpGRUPredictorTorch.py
+++ Python/SpGRUPredictorTorch.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-=======
-# Example: python3 LSTMTSModel.py '../DataSets/signal_with_anomaly.csv' 300 20 3400 0.15 16
-# Example: runfile("LSTMTSModel.py","'../DataSets/signal_with_anomaly.csv' 300 20 3400 0.15 16")
-# Example: runfile("LSTMTSModel.py","'../DataSets/signal_with_anomaly.csv' 30 60 880 0.1 16")
-import sys
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
 """
 Created on Wed Mar 31 02:57:52 2021
 SPGRUPREDICTOR GRU based sparse predictor
    Code by Fredy Vides
-<<<<<<< HEAD
    For Paper, "Computing Sparse Semilinear Models for Approximately Eventually Periodic Signals"
-=======
-   For Paper, "Computing Sparse Autoencoders and Autoregressors for Signal Identification"
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
    by F. Vides
 @author: Fredy Vides
 """
@@ -26,20 +15,12 @@ def SpGRUPredictorTorch(data,model,h,N):
     X = []
     x0 = []
     X = h
-<<<<<<< HEAD
     x0 = FloatTensor(reshape(h.copy(),(1,1,Lag)))
-=======
-    x0 = FloatTensor(reshape(h,(1,1,Lag)))
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
     for j in range(N):
         x = model(x0)
         xc = x0.clone()
         x0[0][0][:-1] = xc[0][0][1:]
         x0[0][0][-1]=x.detach()
         X = append(X,x.detach().numpy()[0][0])
-<<<<<<< HEAD
     X = Md*X+md
-=======
-    X = append(data[0],Md*X+md)
->>>>>>> 0bc613ea8fe7f2056b63b55a534552d93958825b
     return X
